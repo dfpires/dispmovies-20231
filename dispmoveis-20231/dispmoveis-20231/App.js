6a99cd0ab4c32408e45f574fe5687cf14ad9dc77
Daniel Pires <dfpires@gmail.com>
2023-02-16 22:21:14 -0300

    Add files via upload

---
+++ dispmoveis-20231/dispmoveis-20231/App.js
@@ -0,0 +1,8 @@
+// import HelloWorld1 from './components/HelloWorld1'
+import HelloWorld2 from './components/HelloWorld2'
+
+export default function App(){
+
+  // return <HelloWorld1/>
+   return <HelloWorld2/>
+} 
