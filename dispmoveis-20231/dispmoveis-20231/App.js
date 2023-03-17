b0013932094891146331dcf47913144018f97913
Daniel Pires <dfpires@gmail.com>
2023-03-16 22:23:49 -0300

    Add files via upload

--- dispmoveis-20231/dispmoveis-20231/App.js
+++ dispmoveis-20231/dispmoveis-20231/App.js
@@ -1,13 +1,15 @@
 // import HelloWorld1 from './components/HelloWorld1'
 //import HelloWorld2 from './components/HelloWorld2'
-
-import FacebookLogin from "./components/FacebookLogin";
-
+//import FacebookLogin from "./components/FacebookLogin";
 //import TheLight from './components/TheLight'
+
+import SignUp from "./components/SignUp";
+
 export default function App(){
 
   // return <HelloWorld1/>
   // return <HelloWorld2/>
   // return <TheLight/>
-  return <FacebookLogin/>
+  // return <FacebookLogin/>
+  return <SignUp/>
 } 
